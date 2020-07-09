fc2a9a440fd95cffbeb6d84b526638b1fcdd2389
NAMI St. Tammany <namisttammany@gmail.com>
2020-07-09 15:37:37 -0500

    sponsor page ion-avatar

--- src/app/language-modal/language-modal.module.ts
+++ src/app/language-modal/language-modal.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { LanguageModalPageRoutingModule } from './language-modal-routing.module';
-
 import { LanguageModalPage } from './language-modal.page';
 import { TranslateModule } from '@ngx-translate/core';
 
